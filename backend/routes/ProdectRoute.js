86680248561409685a3a8b7a589276f6d7ee49d1
ajarpriyatam <ajar.priyatam.786@gmail.com>
2025-09-28 14:27:42 +0530

    Add delete product route to ProductRoute.js

--- backend/routes/ProdectRoute.js
+++ backend/routes/ProdectRoute.js
@@ -21,8 +21,10 @@ router
   .post(createProduct);
   // .post(isAuthenticatedUser,authorizeRoles("admin"), createProduct);
 
-// router
-//   .route("/admin/product/:id")
-//   .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
-//   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+router
+  .route("/admin/product/:id")
+  // .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(deleteProduct);
+  // .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  // .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 module.exports = router;
